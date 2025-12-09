6c5cd927f1c86903915377479640781a0ebed928
47magnum <milicevicmladen000@gmail.com>
2025-12-09 13:46:28 +0100

    Changed filesize limit

--- backend/server.js
+++ backend/server.js
@@ -25,8 +25,9 @@ app.use(cors({
 
 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// ADD THESE
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
 // Supabase client (service role key must be stored in Render env, not in frontend)
 const SUPABASE_URL = process.env.SUPABASE_URL;
